be95ff41e4ea836b4e2958270cc8f172e6020991
Sarthak Singh <sarthak91singh@gmail.com>
2021-04-26 01:25:53 +0530

    Update polyfill.js

--- polyfill.js
+++ polyfill.js
@@ -1,67 +1,59 @@
-class MyPromise{
+class MyPromise {
 
 
-	constructor(cb){
-  	this.state = "PENDING";
-    this.value = null;
-    this.settled = false;
-    
-    const resolver = (value) => {
-    	if(this.settled) 
-      	return;
-        
-    	this.value = value;
-      this.state = "FULFILLED";
-      this.settled = true;
-      
-      this.thenFn(this.value);
-      this.finallyFn();
+    constructor(cb) {
+        this.state = "PENDING";
+        this.value = null;
+        this.settled = false;
+
+        const resolver = (value) => {
+            if (this.settled)
+                return;
+
+            this.value = value;
+            this.state = "FULFILLED";
+            this.settled = true;
+
+            this.thenFn(this.value);
+            this.finallyFn();
+        }
+
+        const rejector = (value) => {
+            if (this.settled)
+                return;
+
+            this.value = value;
+            this.state = "REJECTED";
+            this.settled = true;
+
+            this.catchFn(this.value);
+            this.finallyFn();
+        }
+
+        try {
+            cb(resolver, rejector);
+        } catch (e) {
+            rejector(e);
+        }
     }
-    
-    const rejector = (value) => {
-    	if(this.settled) 
-      	return;
-        
-    	this.value = value;
-      this.state = "REJECTED";
-      this.settled = true;
-      
-      this.catchFn(this.value);
-      this.finallyFn();
+
+
+    then(thenFn) {
+        if (typeof thenFn == "function") {
+            this.thenFn = thenFn;
+        }
+        return this;
     }
-    
-    try{
-    	cb(resolver, rejector);
+
+    catch (catchFn) {
+        if (typeof catchFn == "function") {
+            this.catchFn = catchFn;
+        }
+        return this;
+    } finally(finallyFn) {
+        if (typeof finallyFn == "function") {
+            this.finallyFn = finallyFn;
+        }
     }
-    catch(e){
-    	rejector(e);
-    }    
-  }
-  
-  
-  then(thenFn){
-  	if(typeof thenFn == "function"){
-    		this.thenFn = thenFn;
-    }
-    return this;
-  }
-  
-  catch(catchFn){
-  	if(typeof catchFn == "function"){
-    		this.catchFn = catchFn;
-    }
-    return this;
-  }
-  
-  finally(finallyFn){
-  	if(typeof finallyFn == "function"){
-    	this.finallyFn = finallyFn;
-    }
-  }
-  
+
 }
-
-
-
-
-
